000d7c5d45be62ad2f80e69f1f02fde867113c89
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-02-19 17:59:56 -0600

    nuevo diseño de login

---
+++ frontend/src/app/components/Login/AuthFooter.jsx
@@ -0,0 +1,14 @@
+import { Link, Typography, Stack } from '@mui/material';
+
+const AuthFooter = () => (
+    <Stack direction="row" justifyContent="space-between">
+    <Typography variant="subtitle2" component={Link} href="https://www.plataformadigitalnacional.org" target="_blank" underline="hover">
+    plataformadigitalnacional.org
+    </Typography>
+    <Typography variant="subtitle2" component={Link} href="https://www.sna.org.mx" target="_blank" underline="hover">
+    sna.org.mx
+    </Typography>
+  </Stack>
+);
+
+export default AuthFooter;

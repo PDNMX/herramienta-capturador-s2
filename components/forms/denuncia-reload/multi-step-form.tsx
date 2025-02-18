904726ff0087b12595476f004e21a71d710e163d
RBalan21 <aln.rojas.pdn@gmail.com>
2025-02-18 16:46:16 -0600

    forms update titles

--- components/forms/denuncia-reload/multi-step-form.tsx
+++ components/forms/denuncia-reload/multi-step-form.tsx
@@ -150,7 +150,7 @@ export function MultiStepForm() {
         <div className="border-b">
           <div className="container mx-auto px-4 py-6">
             <div className="flex justify-between items-center mb-6">
-              <h2 className="text-3xl font-bold text-primary">{steps[step].title}</h2>
+              <h2 className="text-3xl font-bold foregroundy">{steps[step].title}</h2>
               <Sheet>
                 <SheetTrigger asChild>
                   <Button variant="outline" size="icon" className="rounded-full">
